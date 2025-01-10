b547847856cbc95f2c624b648d4ab1c4d52ae054
Mikulash <62067494+mikulash@users.noreply.github.com>
2025-01-10 21:53:14 +0100

    feat: tools update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.github.mikulash.shareasqr"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -47,6 +47,7 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    buildToolsVersion = "35.0.0"
 }
 
 dependencies {
