94abd121d42232f9ffdfe54ddae5e83385c4b162
Mikulash <62067494+mikulash@users.noreply.github.com>
2025-01-10 23:17:49 +0100

    feat: gradle build update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,13 +11,18 @@ android {
         applicationId = "com.github.mikulash.shareasqr"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
+    }
+
+    dependenciesInfo {
+        includeInApk = false
+        includeInBundle = false
     }
 
     buildTypes {
