37d61125ac836807b9bf71b21a952f87c93fc865
Mikulash <62067494+mikulash@users.noreply.github.com>
2025-01-10 21:32:26 +0100

    feat: app id update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.shareasqr"
+        applicationId = "com.github.mikulash.shareasqr"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
